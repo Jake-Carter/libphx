549f110ad1e71669fbf64efd6d030e36568b7293
Jake Carter <jhcarter.co@gmail.com>
2026-06-27 16:51:10 -0700

    Skip BSP_Create when mesh validation fails
    Return nil from BSP.Create instead of calling into native BSP_Create on
    invalid meshes. Avoids fatal errors on proc-gen ships that fail
    Mesh_Validate while still building BSP for good meshes.

--- script/ffiext/BSP.lua
+++ script/ffiext/BSP.lua
@@ -1,13 +1,13 @@
 local libphx = require('ffi.libphx').lib
 
 function onDef_BSP (t, mt)
-  t.Create = function (...)
-    local e = libphx.Mesh_Validate(...)
+  t.Create = function (mesh)
+    local e = libphx.Mesh_Validate(mesh)
     if e ~= 0 then
       print('BSP Incoming Mesh Error:')
       libphx.Error_Print(e)
+      return nil
     end
-    local result = libphx.BSP_Create(...)
-    return result
+    return libphx.BSP_Create(mesh)
   end
 end
